05bd38b216e35e399ae55ebfbe120d9ff4e9614c
Moravy <moravy22@gmail.com>
2020-06-27 15:07:42 +1200

    Add comments on each function, create three extra functions: HandleDelete, HandleSubmit and HandleAdd

--- src/Container/todoItem.jsx
+++ src/Container/todoItem.jsx
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 class Buttons extends Component {
     state = {  }
 
-    // handleAdd = () => {
-    //     console.log(this.props.id)
-    // }
-
+    //2.
+    // I passed Item (this.props.values) so that I can return the span description
+    // Create two buttons where one called the function handleDelete with parameter of (this.props.id)
     createTask(item){
-        // console.log(item)
         return <React.Fragment>
             <span className="presentation"><h5>{item}</h5></span>
             <div className= "buttons">          
-                <button type="button" onClick={this.handleAdd} className="b add btn btn-success"><i className="fa fa-check"></i></button>
+                <button type="button" onClick={this.handleAdd} className="b btn btn-success"><i className="fa fa-check"></i></button>
                 <button type="button" onClick={() => this.props.onDelete(this.props.id)}className="b btn btn-danger"><i className="fa fa-trash"></i></button>
             </div>
         </React.Fragment>
     }
+    
+    
     render() { 
+        //1.
+        //In todo_list.jsx I pass abunch of "parameter"(idk what its call) eg. key={something}, this allow us to retrieve information from TodoList
+        //So you can do this.props.value, this.props.key
+        // then I call createTask 
         var todoEntries = this.props.value;
         var Items = this.createTask(todoEntries);
  
--- src/Container/todo_list.jsx
+++ src/Container/todo_list.jsx
@@ -3,21 +3,41 @@ import Todoitem from "./todoItem";
 class todo_list extends Component {
     constructor(props) {
         super(props);
-        // this.handleadd = this.handleadd.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
         this.state = {
+          value: '',
           data: [],
+          id: null,
         };
       }
+
+    // Fetch all the todoItem from the APi and setState
     componentDidMount(){
-        fetch('http://localhost:8080/users')
-        .then(response => response.json())
-        .then(fromApi => {
-          this.setState({data:fromApi})
-
-        });
-
+        // fetch('http://localhost:8080/users')
+        // .then(response => response.json())
+        // .then(fromApi => {
+        //   this.setState({data:fromApi})
+        // });
+        this.handleAdd();
     }
 
+    handleAdd(){
+      fetch('http://localhost:8080/users')
+      .then(response => response.json())
+      .then(fromApi => {
+        this.setState({data:fromApi})
+        fromApi.map(element =>{
+          this.setState({id:element.id})
+        })
+      });
+    }
+    //When the delete this function is triggered
+    //This function has a parameter of itemId. This parameter will be used to filter the this.state.data
+    //the filter is if the parameter is if current element.id "not equal value or not equal type" itemId
+    //Once done setState to a new list (Since react recommend to think state is immutable)
+    //After we delete the item from PSQL using API(FROM pgAPi.js) using DELETE.
     handleDelete = itemId =>{
       const items = this.state.data.filter((element) => element.id !== itemId);
       this.setState({data:items})
@@ -28,6 +48,11 @@ class todo_list extends Component {
       .then(res => console.log(res))
     }
 
+
+    // Dynamically create new component depend on how many item there are on the list.
+    // Passing keys, id will allow me to identify which object this is.
+    // Passing value, so each of the component knows what todoItem it is
+    // Passing Delete allow the todoItem to use THIS.handleDelete
     renderData(){
       if (this.state.data.length === 0) return <p>You don't have anything to do today</p>
       return  <React.Fragment>
@@ -36,13 +61,41 @@ class todo_list extends Component {
                 ))}
               </React.Fragment>
     }
-    //FINISHED ADDING DELETE
-    // trying adding a add function
-    // trying to add object into data
 
+    handleChange(event) {
+      this.setState({value:event.target.value})
+      }
+
+    //FIX INSERTING 
+
+    handleSubmit(event) {
+      event.preventDefault();
+      this.setState({id:(parseInt(this.state.id)+1).toString()})
+      fetch('http://localhost:8080/users/',{
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({id:this.state.id,todo: this.state.value })
+        })
+      const added = this.state.data.concat({id:this.state.id,todo: this.state.value })
+      this.setState({data:added})
+      console.log(this.state.data)
+      // const added = this.state.data.concat({id:"50",todo:this.state.value})
+      // this.setState({data:added})
+      }
+
+    
     render() { 
         return (
           <React.Fragment>
+          <form  onSubmit={this.handleSubmit}>
+            <input value={this.state.value} onChange={this.handleChange} type="text" className="add"/>
+            <div className="buttons">
+              <button type="submit" className="b btn btn-success"><i className="fa fa-plus"></i></button>
+              <button type="button" className="b disable " disabled><i className="fa fa-plus"></i></button>
+            </div>
+          </form>
+          
+          
           {this.renderData()}
           </React.Fragment>
          );
