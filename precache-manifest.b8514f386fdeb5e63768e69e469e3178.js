fc34af2ab43d1fcf7104fb2d58324251fe1c36ff
Moravy <moravy22@gmail.com>
2020-07-01 23:03:18 +1200

    Updates

---
+++ precache-manifest.b8514f386fdeb5e63768e69e469e3178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47031318f750f82de745eb01eb32979a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e7a3e5afa367067c4ec2",
+    "url": "/todolist/static/css/main.aefe965f.chunk.css"
+  },
+  {
+    "revision": "2d6e5dd3381a9562c93a",
+    "url": "/todolist/static/js/2.92a90600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.92a90600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a3e5afa367067c4ec2",
+    "url": "/todolist/static/js/main.c128e2d5.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
